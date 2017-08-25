24b7d260be6f7cc82f5900757ed375f603826622
merberg <merrick.dranberg@gmail.com>
2017-08-24 23:19:27 -0400

    left, right, and flipping

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 sampleLines = []
-with open('examples/data/driving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         #Skip the header
@@ -22,24 +22,34 @@ print("{} Validation Samples".format(len(validation_samples)))
 
 # A Generator to read in the data in partitions
 def generator(samples, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
+    num_samples = len(samples)    
+    while 1:
+        samples = shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
             images = []
             angles = []
+            correction = [0, 0.15, -0.15]
             for batch_sample in batch_samples:
-                name = 'examples/data/IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                #All the cameras have a normal and flipped entry
+                for camera in range(3):
+                    name = 'data/IMG/'+batch_sample[camera].split('/')[-1]
+                    bgr = cv2.imread(name)
+                    rgb = bgr[...,::-1]
+                    angle = float(batch_sample[3]) + correction[camera]
+                    images.append(rgb)
+                    angles.append(angle)
 
+                    flipped_image = cv2.flip(rgb, 1)
+                    flipped_angle = -angle
+                    images.append(flipped_image)
+                    angles.append(flipped_angle)
+
+            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
+            yield shuffle(X_train, y_train)
 
 # Generators for training and validation
 training_generator = generator(training_samples, batch_size=32)
@@ -66,10 +76,22 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(training_generator,
+history_object = model.fit_generator(training_generator,
                     samples_per_epoch=len(training_samples),
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples),
-                    nb_epoch=3)
+                    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
